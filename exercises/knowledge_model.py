25ecdb410e8f8a7814456f6237b5b913719b4e76
= <=>
2018-08-06 12:19:55 +0300

    k

--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -5,13 +5,23 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Knowledge(Base):
+class User(Base):
 	# Create a table with 4 columns
+	__tablename__ = 'Users'
 	# The first column will be the primary key
+	User_id = Column(Integer, primary_key=	True)
 	# The second column should be a string representing
+	Name = Column(String)
 	# the name of the Wiki article that you're referencing
+	BirthDay = Column(Integer)
 	# The third column will be a string representing the 
+	Active = Column(Boolean)
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
-
-	pass
+	def __repr__(self):
+		return ("User_Name: {}\n"
+				"User_BirthDay: {}\n"
+				"Activity: {}\n").format(
+				self.Name, self.BirthDay, self.Active)
+		
+	
